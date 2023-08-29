9a36bb9954fc958a31e445bab74d4b7930eea8f8
“DasNeve” <henriquemauriciomagnus@gmail.com>
2023-08-29 11:52:05 -0300

    calculadora matador de porco

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,9 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multiplica = require ("./modulos/multiplica");
+var divisao = require ("./modulos/divisao");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(multiplica(20,90));
+console.log(divisao(20,90));
---
+++ modulos/multiplica.js
@@ -0,0 +1,4 @@
+var multiplica = function (x,y) {
+    return x * y;
+  }; 
+  module.exports = multiplica;
